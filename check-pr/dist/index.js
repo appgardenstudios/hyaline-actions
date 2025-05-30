97c36f6bf35ebddd97ff59f5df5d03afd162b49d
John Clark <socrates37@gmail.com>
2025-05-30 12:53:17 -0600

    basics

--- check-pr/dist/index.js
+++ check-pr/dist/index.js
@@ -27564,14 +27564,36 @@ async function check () {
     const repository = core.getInput('repository');
     const pull_number = core.getInput('pull_number');
     const github_token = core.getInput('github_token');
-    // TODO
-    console.log(config, repository, pull_number);
-
+    
+    // Get owner and repo from inputs, falling back to context
+    let [owner, repo] = repository.split('/');
+    if (!owner || !repo) {
+      [owner, repo] = github.context.repository.split('/');
+    }
+    console.log(`Checking PR ${owner}/${repo}/${pull_number} using config ${config}`);
+    
     // Get HEAD/BASE for Pull Request
-    // TODO
+    const octokit = github.getOctokit(github_token);
+    const { data: pullRequest } = await octokit.rest.pulls.get({
+        owner,
+        repo,
+        pull_number,
+    });
+    const head = pullRequest.head.ref;
+    const base = pullRequest.head.base;
+    console.log(`Using head: ${head}, base: ${base}`);
 
     // See if a Hyaline comment already exists
-    // TODO
+    const { data: comments } = await octokit.rest.issues.listComments({
+      owner,
+      repo,
+      issue_number: pull_number,
+      per_page: 100, // TODO loop to get them all
+    });
+    let commentID = comments.find((comment) => {
+      return comment.body.startsWith('# H\u200By\u200Ba\u200Bl\u200Bi\u200Bn\u200Be');
+    });
+    console.log(`Using comment: ${commentID}`);
 
     // Run extract current
     // TODO

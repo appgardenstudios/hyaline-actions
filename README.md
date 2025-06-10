3188d571475ed4c54cc71a8b3297c9bd6092bd5f
John Clark <socrates37@gmail.com>
2025-06-10 07:45:13 -0600

    Open Beta - integrations - check pr - github action (#2)

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ GitHub Actions for [Hyaline](https://github.com/appgardenstudios/hyaline).
 
 ## appgardenstudios/hyaline-actions/setup
 This action installs and sets up the Hyaline CLI in your GitHub action workflow. Please visit the action's [README.md](./setup/README.md) for more information.
+
+## appgardenstudios/hyaline-actions/check-pr
+This action extracts and checks the change associated with a PR in your GitHub action workflow. Please visit the action's [README.md](./check-pr/README.md) for more information.
---
+++ check-pr/index.js
@@ -0,0 +1,150 @@
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const github = require('@actions/github');
+
+async function check() {
+  try {
+    // Get inputs
+    const config = core.getInput('config');
+    const system = core.getInput('system');
+    const repository = core.getInput('repository');
+    const pr_number = core.getInput('pr_number');
+    const github_token = core.getInput('github_token');
+    
+    // Generate run UUID
+    const uuid = crypto.randomUUID();
+    console.log(`Using run UUID ${uuid}`);
+
+    // Get owner and repo from inputs, falling back to context
+    let [owner, repo] = repository.split('/');
+    if (!owner || !repo) {
+      [owner, repo] = github.context.repository.split('/');
+    }
+    console.log(`Checking PR ${owner}/${repo}/${pr_number} using system ${system} and config ${config}`);
+    
+    // Get HEAD/BASE for Pull Request
+    const octokit = github.getOctokit(github_token);
+    const { data: pullRequest } = await octokit.rest.pulls.get({
+        owner,
+        repo,
+        pull_number: pr_number,
+    });
+    const head = pullRequest.head.ref;
+    const sha = pullRequest.head.sha;
+    const base = pullRequest.base.ref;
+    console.log(`Using head: ${head}, base: ${base}`);
+
+    // See if a Hyaline comment already exists
+    let commentID = undefined;
+    for await (const { data: comments } of octokit.paginate.iterator(
+      octokit.rest.issues.listComments,
+      {
+        owner,
+        repo,
+        issue_number: pr_number,
+        per_page: 100,
+      },
+    )) {
+      const comment = comments.find((comment) => {
+        return comment.body.startsWith('# H\u200By\u200Ba\u200Bl\u200Bi\u200Bn\u200Be');
+      });
+      if (comment) {
+        commentID = comment.id;
+        break;
+      }
+    }
+    console.log(`Using comment: ${commentID}`);
+
+    // Run version
+    console.log('Running hyaline version:');
+    await exec.exec('hyaline', ['version']);
+
+    // Run extract current
+    console.log('Running hyaline extract current:');
+    await exec.exec('hyaline', [
+      '--debug',
+      'extract', 'current',
+      '--config', config,
+      '--system', system,
+      '--output', `./current-${uuid}.db`,
+    ]);
+
+    // Run extract change
+    console.log('Running hyaline extract change:');
+    await exec.exec('hyaline', [
+      '--debug',
+      'extract', 'change',
+      '--config', config,
+      '--system', system,
+      '--base', `origin/${base}`,
+      '--head', `origin/${head}`,
+      '--output', `./change-${uuid}.db`,
+    ]);
+
+    // Run check change
+    console.log('Running hyaline check change:');
+    await exec.exec('hyaline', [
+      '--debug',
+      'check', 'change',
+      '--config', config,
+      '--system', system,
+      '--current', `./current-${uuid}.db`,
+      '--change', `./change-${uuid}.db`,
+      '--output', `./recommendations-${uuid}.json`,
+    ]);
+
+    // Run update pr
+    console.log('Running hyaline update pr:');
+    const updatePR = [
+      '--debug',
+      'update', 'pr',
+      '--config', config,
+      '--pull-request', `${owner}/${repo}/${pr_number}`,
+      '--sha', sha,
+      '--recommendations', `./recommendations-${uuid}.json`,
+      '--output', `./comment-${uuid}.json`,
+    ];
+    if (commentID) {
+      updatePR.push('--comment', `${owner}/${repo}/${commentID}`);
+    }
+    await exec.exec('hyaline', updatePR);
+
+    // Set outputs
+    console.log('Setting Outputs:');
+    const commentMetadataPath = path.join(process.cwd(), `./comment-${uuid}.json`);
+    console.log(`Loading comment metadata from ${commentMetadataPath}`);
+    const rawCommentMetadata = fs.readFileSync(commentMetadataPath, 'utf8');
+    const commentMetadata = JSON.parse(rawCommentMetadata);
+    let completed_recommendations = 0;
+    let outstanding_recommendations = 0;
+    let total_recommendations = commentMetadata.recommendations?.length || 0;
+    commentMetadata.recommendations?.forEach(rec => {
+      if (rec.checked) {
+        completed_recommendations++;
+      } else {
+        outstanding_recommendations++;
+      }
+    });
+    console.log(`completed_recommendations: ${completed_recommendations}`);
+    core.setOutput("completed_recommendations", completed_recommendations);
+    console.log(`outstanding_recommendations: ${outstanding_recommendations}`);
+    core.setOutput("outstanding_recommendations", outstanding_recommendations);
+    console.log(`total_recommendations: ${total_recommendations}`);
+    core.setOutput("total_recommendations", total_recommendations);
+
+  } catch (error) {
+    core.error(error);
+    throw error;
+  }
+}
+
+(async () => {
+  try {
+    await check();
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
